629c47fcff3ad58319d52106f6bca96122eee51a
selin yıldırım <sy248359@gmail.com>
2025-02-07 10:47:57 +0300

    Generic list ve Kampintro3 güncellendi

--- Cenericsintro/MyList.cs
+++ Cenericsintro/MyList.cs
@@ -8,15 +8,20 @@ namespace Cenericsintro
 {
     class MyList<T>
     {
-        T items;
+        T[] items;
         //constructor:
         public MyList(T item)
         {
-            items = new T[];
+            items = new T[0];//bir clas newlendiğinde çalışan bloğa constructor denir.
 
         }
-        public void Add(T item) { 
-
+        public void Add(T item) {
+            T[] tempArry = items;
+            items = new T[items.Length + 1];
+            for (int i = 0; i < tempArry.Length; i++) {
+                items[i] = tempArry[i];
+            }
+            items[items.Length - 1]=item;
         }
     }
 }
